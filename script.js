c1627c26ef1edb7944d3e190d1bb090e620a424b
HopeBvumbwe <hopebvumbwe@gmail.com>
2026-06-18 12:09:03 +0200

    JS edited

---
+++ script.js
@@ -0,0 +1,19 @@
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide() {
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[currentSlide].classList.add("active");
+
+    currentSlide++;
+
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+}
+
+showSlide();
+setInterval(showSlide, 3000);
